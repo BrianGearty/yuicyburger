0ac901e7d1eaca50e046e4339e81dc276a3b0351
Brian GEARTY <geartybrian@gmail.com>
2020-09-30 19:27:32 -0400

    messing with connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql");
 
+var connection;
+
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -13,6 +15,13 @@ if (process.env.JAWSDB_URL) {
 });
 }
 
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
-connection.connect();
+
 module.exports = connection;
